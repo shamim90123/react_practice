fafc95afae3a0aa46d4b882d18438b43e6ae72f0
Md Shamim Reza <opt.shamim@gmail.com>
2025-06-23 08:37:01 +0600

    code reivew

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
-// import styles from './Counter.module.css'
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
